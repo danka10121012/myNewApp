de89d5856bde3e7dceb068614775dcf141a9524d
danka10121012 <91868920+danka10121012@users.noreply.github.com>
2022-04-06 22:20:29 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { MyStack } from "./navigation/navigator";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return <MyStack />;
+}
